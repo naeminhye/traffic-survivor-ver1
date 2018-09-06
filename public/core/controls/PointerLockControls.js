7fd676416508250ea2f73ba066da6563eb46c43e
Hieu Nguyen <hieunm.citynow@gmail.com>
2018-09-07 00:28:05 +0700

    fix name

--- public/core/controls/PointerLockControls.js
+++ public/core/controls/PointerLockControls.js
@@ -81,67 +81,6 @@ var PointerLockControls = function (camera, cannonBody) {
         pitchObject.rotation.x = Math.max(- PI_2, Math.min(PI_2, pitchObject.rotation.x));
     };
 
-    // var onKeyDown = function (event) {
-
-    //     switch (event.keyCode) {
-
-    //         case 38: // up
-    //         case 87: // w
-    //             moveForward = true;
-    //             break;
-
-    //         case 37: // left
-    //         case 65: // a
-    //             moveLeft = true;
-    //             break;
-
-    //         case 40: // down
-    //         case 83: // s
-    //             moveBackward = true;
-    //             break;
-
-    //         case 39: // right
-    //         case 68: // d
-    //             moveRight = true;
-    //             break;
-
-    //         case 32: // space
-    //             if (canJump === true) {
-    //                 velocity.y = jumpVelocity;
-    //             }
-    //             canJump = false;
-    //             break;
-    //     }
-
-    // };
-
-    // var onKeyUp = function (event) {
-
-    //     switch (event.keyCode) {
-
-    //         case 38: // up
-    //         case 87: // w
-    //             moveForward = false;
-    //             break;
-
-    //         case 37: // left
-    //         case 65: // a
-    //             moveLeft = false;
-    //             break;
-
-    //         case 40: // down
-    //         case 83: // a
-    //             moveBackward = false;
-    //             break;
-
-    //         case 39: // right
-    //         case 68: // d
-    //             moveRight = false;
-    //             break;
-
-    //     }
-
-    // };
     /** Keyboard events */
     var onKeyDown = function ( event ) {
         switch ( event.keyCode ) {
